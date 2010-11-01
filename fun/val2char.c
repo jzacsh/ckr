71aae0018aa2240ca7f1cd0c26ed6e1df0319480
Jonathan Zacsh <jzacsh@gmail.com>
2010-11-01 19:31:05 -0400

    added debug statements

--- fun/val2char.c
+++ fun/val2char.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define DELIM 32 //character delimiter
 
 int main() {
   int c;
@@ -7,10 +9,18 @@ int main() {
   char *ptr = ch;
 
   while ((c = getchar()) != EOF) {
-    if (c == 32)
-      printf("%s", ch);
+    if (c == DELIM) {
+      printf("DBG: a space has been found\nconverting current ch (%c) to integer.\n", ch);
+      int num;
+      num = atoi(ch);
+      printf("DBG: char string has been converted to integer %d\n", num);
+      printf("%d", num);
+      ch[0] = ch[1]= ch[2] = 0;
+      ptr = &ch[0];
+    }
     else
       *ptr++ = c;
+      printf("DBG: char, so far, is %s\n", ch);
   }
 
   printf("\n");
