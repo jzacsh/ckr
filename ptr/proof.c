ac3b6ed9e49c76e13f11361e1827f17d2a74261b
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-01 22:48:48 -0500

    proper proof of two seperate things.

--- ptr/proof.c
+++ ptr/proof.c
@@ -9,22 +9,10 @@ int alterptr(int y) {
 }
 
 int main(int argc, char *argv[], char *envp[]) {
-    int x = 3, i = 0, n = 0;
+    int x = 3;
     int *ptr = &x;
-    char *env;
 
     printf("x is pointing to: %d\npointer points to value of: %d\n", x, *ptr);
 
-    //env vars
-    printf("envp, printed here:\n");
-    while (env = envp[i++])
-        printf("envp[%d] = %s\n", i, envp[i]);
-
-    //args
-    printf("argv, printed here:\n");
-    if (argc > 1)
-        for (n; n < argc; n++)
-            printf("argv[%d] = %s\n", n, argv[n]);
-    else
-        printf("no parameters were passed!\n");
+    return 0;
 }
