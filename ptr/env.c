3f58bdadc2311d3c77a8274083cb4ead42c24a0c
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-02 09:31:03 -0500

    debugging output, and functional getopts usage

--- ptr/env.c
+++ ptr/env.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int DEBUG = 1;
+
 /**
  * Remake of `env`
  */
 int main (int argc, char *argv[], char *envp[]) {
     int i = 0;
     char *env;
+    int opts = getopt(argc, argv, "i,0,u");
 
-    /*
-     * why can't i _just_ comment out `printf`?
-     *
-    if (argc == 1)
+    if (opts == -1)
         while (env = envp[i++])
             printf("%s\n", env);
-    */
 
-
-    int opts = getopt(argc, argv, "i,0,u");
-    printf("opts = %d\n", opts);
+    if (DEBUG) printf("DEBUG: opts = %d\n", opts);
 
     return 0;
 }
