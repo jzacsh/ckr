b26a75338ac5c2bd5cb1fc0ef96041bc37a0a1b7
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-01 22:35:43 -0500

    understanding of the provided envp array of char pointers; printing all provided args and env vars.

--- ptr/proof.c
+++ ptr/proof.c
@@ -8,9 +8,23 @@ int alterptr(int y) {
     return 0;
 }
 
-int main(int argc, char **argv) {
-    int x = 3;
+int main(int argc, char *argv[], char *envp[]) {
+    int x = 3, i = 0, n = 0;
     int *ptr = &x;
+    char *env;
 
     printf("x is pointing to: %d\npointer points to value of: %d\n", x, *ptr);
+
+    //env vars
+    printf("envp, printed here:\n");
+    while (env = envp[i++])
+        printf("envp[%d] = %s\n", i, envp[i]);
+
+    //args
+    printf("argv, printed here:\n");
+    if (argc > 1)
+        for (n; n < argc; n++)
+            printf("argv[%d] = %s\n", n, argv[n]);
+    else
+        printf("no parameters were passed!\n");
 }
