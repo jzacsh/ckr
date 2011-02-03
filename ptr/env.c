248a94d4df18186075ebc2a5eaf9cd3d893034b6
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-03 18:46:56 -0500

    full understanding of getopt, broken down

--- ptr/env.c
+++ ptr/env.c
@@ -7,7 +7,7 @@ int DEBUG = 1;
  * Remake of `env`
  */
 int main (int argc, char *argv[], char *envp[]) {
-    int i = 0;
+    int i = 0, x = 0;
     char *env;
     int opt;
 
@@ -16,8 +16,15 @@ int main (int argc, char *argv[], char *envp[]) {
     if (argc == 1)
         while (envp[i]) printf("%s\n", envp[i++]);
 
+    if (DEBUG) printf("DEBUG: below is the current argv[]\n");
+    while (argv[x]) {
+        if (DEBUG) printf("DEBUG: argv[%d] = %s\n", x, argv[x]);
+        x++;
+    }
+
     while ((opt = getopt(argc, argv, "i0u")) != -1) {
-        if (DEBUG) printf("DEBUG: opt is: %c\n", opt);
+        if (DEBUG) printf("DEBUG: opt is: %c; optind is: %d\n", opt, optind);
+        if (DEBUG) printf("argv[optind(%d)] = %s\n", optind, argv[optind]);
         switch (opt) {
             case 'i':
                 printf("implement `%c`!\n", opt);
@@ -29,9 +36,17 @@ int main (int argc, char *argv[], char *envp[]) {
                 printf("implement `%c`!\n", opt);
                 break;
             default:
-                printf("you ain't doing n0thin, boy!\n");
+                printf("ain't n0 such thang, boy!\n");
                 break;
         }
+    }
+
+    if (DEBUG) printf("DEBUG: optind = %d\n", optind);
+    x = 0;
+    if (DEBUG) printf("DEBUG: below is the current argv[]\n");
+    while (argv[x]) {
+        if (DEBUG) printf("DEBUG: argv[%d] = %s\n", x, argv[x]);
+        x++;
     }
 
     return 0;
--- ptr/proof.c
+++ ptr/proof.c
@@ -9,10 +9,13 @@ int alterptr(int y) {
 }
 
 int main(int argc, char *argv[], char *envp[]) {
-    int x = 3;
+    int x = 3, i = 0;
     int *ptr = &x;
 
     printf("x is pointing to: %d\npointer points to value of: %d\n", x, *ptr);
+
+    while (argv[i++])
+        printf("DEBUG: argv[%d] = %s\n", i-1, argv[i-1]);
 
     return 0;
 }
