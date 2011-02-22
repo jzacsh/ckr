c7642c82070cd72b8b4639fae36bf4e9fe100c03
Jonathan Zacsh <jzacsh@gmail.com>
2011-02-22 17:49:32 -0500

    attempts at pointer logic, from memory (human).

--- ptr/list.c
+++ ptr/list.c
@@ -1,11 +1,25 @@
 #include<stdio.h>
 
-struct {
-    char *name[],
-    int **next,
-} li;
+typedef struct li_t {
+    int value;
+    struct li_t *next;
+} item;
 
 int main(int argc, char *argv[], char *envp[])
 {
-//  li itemone;
+    item * head, *current;
+    int i, list_size = 10;
+
+    head = NULL;
+
+    printf("Will create a %d item list.\n", list_size);
+    item contacts = * malloc(sizeof(item));
+
+    for (i = 0; i < list_size; i++) {
+        printf(".. creating number #%2.d in the list.\n", i+1);
+        nums->next = head;
+        nums->value = i;
+    }
+
+    return(0);
 }
